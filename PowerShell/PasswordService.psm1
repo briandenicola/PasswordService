d1fbc7bed6d28ad72ebca49b6ec5b7d47493a87c
Brian Denicola <bjd145@yahoo.com>
2014-11-03 07:52:05 -0600

    Version 0.0.6
    Powershell Module
    Renamed Folders

---
+++ PowerShell/PasswordService.psm1
@@ -0,0 +1,64 @@
+﻿#requires -version 3
+
+Set-Variable -Name service_credentials -Value ([System.Management.Automation.PSCredential]::Empty) -Option AllScope
+Set-Variable -Name passwordlist -Value @()
+
+$password_urls = New-Object PSObject -Property @{
+    PasswordList="https://api.password.com/api/passwords"
+    PasswordDetails="https://api.password.com/api/passwords/{0}"
+}
+
+$password_output = New-Object PSObject -Property @{
+}
+
+$password_error_data = New-Object PSObject -Property @{ 
+    NoAppName="No password was found of Name : {0}."  
+    NoAppId="No password was found of ID : {0}."
+}
+
+function __Get-PasswordServiceCredentials 
+{
+    if($service_credentials -eq [System.Management.Automation.PSCredential]::Empty) {
+        $service_credentials = Get-Credential ( $env:USERDOMAIN + "\" + $env:USERNAME )
+    }
+    return $service_credentials
+}
+
+function Get-Passwords
+{
+    if( $passwordlist -eq @() ) {
+        $passwordlist = Invoke-RestMethod -Method Get -Uri $password_urls.PasswordList -Credential (__Get-PasswordServiceCredentials)
+    }
+
+    return $passwordlist
+}
+
+function Get-Password
+{
+    param (
+        [int] $id
+    )
+
+    return (Invoke-RestMethod -Method Get -Uri ($password.PasswordDetails -f $id) -Credential (__Get-PasswordServiceCredentials))
+}
+
+function Create-NewPassword
+{
+    param (
+        [Hashtable] $password
+    )
+
+    return (Invoke-RestMethod -Method Post -Uri $password.PasswordList -Credential (__Get-PasswordServiceCredentials) -Body $password)
+}
+
+function Update-Password 
+{
+    param (
+        [int] $id,
+        [Hashtable] $password
+    )
+
+    return (Invoke-RestMethod -Method Put -Uri ($password.PasswordDetails -f $id) -Credential (__Get-PasswordServiceCredentials) -Body ($password | ConvertTo-Json) -ContentType "application/json")
+}
+
+Export-ModuleMember -Function Get-Passwords, Get-Password, Get-passwordChecks, Create-NewPassword, Update-Password 
