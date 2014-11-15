6a14b518c5948eddf87b3f9b3556eef258b6b585
Brian Denicola <bjd145@yahoo.com>
2014-11-15 13:05:14 -0600

    Key Generator for this application

---
+++ Source/PasswordService.KeyGenerator/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+
+namespace PasswordService.KeyGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             int RANDOM_KEY_TRIES = 1000;
+             using (Aes aes = Aes.Create()) {
+                aes.KeySize = 256;
+
+                for (int i=0; i <= RANDOM_KEY_TRIES; i++) {
+                     aes.GenerateKey();
+                 }
+
+                System.Console.WriteLine("Key = {0}", Convert.ToBase64String(aes.Key));
+                System.Console.WriteLine("IV = {0}", Convert.ToBase64String(aes.IV));
+            };
+        }
+    }
+}
