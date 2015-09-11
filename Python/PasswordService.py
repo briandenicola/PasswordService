877933b2d66f752f9c012240bea596ab06942875
Brian Denicola <bjd145@yahoo.com>
2015-09-10 22:20:23 -0500

    Updated Python Class to Include New and Updated Functions

--- Python/PasswordService.py
+++ Python/PasswordService.py
@@ -1,37 +1,58 @@
-﻿import argparse
+import argparse
 import json
 import requests
 import urllib3
 import os
 from requests_ntlm import HttpNtlmAuth
 
+class Password:
+	User  = None
+	Value = None
+	Usage = None
+	def __init__(self, username, password, usage):
+		self.User = username
+		self.Value = password
+		self.Usage = usage
+	
 class PasswordService:
-    server = "server.com"
-    webservice_url = "https://" + server + "/passwords/api/passwords/"
-    auth = None
-    ca_certificate_path = "False" 
-    passwords = None
+	server = "server.com"
+	webservice_url = "https://{0}/passwords/api/passwords/".format(server)
+	auth = None
+	ca_certificate_path = "False" 
+	passwords = None
 
-    def __init__(self, username, password, ca):
-        if os.path.exists(ca):  self.ca_certificate_path = ca
-        self.auth =  HttpNtlmAuth(username,password)
-        # self.ReloadPasswords()
-    
-    def LoadPasswords(self):
-        r = requests.get( self.webservice_url, auth=self.auth, verify=self.ca_certificate_path)
-        if r.status_code == 200:
-            self.passwords = json.loads(r.text)
+	def __init__(self, username, password, ca):
+		if os.path.exists(ca):  self.ca_certificate_path = ca
+		self.auth =  HttpNtlmAuth(username,password)
+	
+	def LoadPasswords(self):
+		r = requests.get( self.webservice_url, auth=self.auth, verify=self.ca_certificate_path )
+		if r.status_code == 200:
+			self.passwords = json.loads(r.text)
 
-    def FindIdByName(self,name):
-        ids = [ password['PasswordId'] for password in self.passwords if password['Name'] == name ]
-        return ids
+	def FindIdByName(self,name):
+		ids = [ password['PasswordId'] for password in self.passwords if password['Name'] == name ]
+		return ids
 
-    def GetPassword(self,id):
-        r = requests.get( self.webservice_url + str(id), auth=self.auth, verify=self.ca_certificate_path)
-        if r.status_code == 200:
-            return (json.loads(r.text))["Value"]
-        else:
-            return None
+	def GetPasswords(self):
+		return [ Password(password['Name'],password['Value'],password['Usage']) for password in self.passwords ]
+		
+	def GetPassword(self,id):
+		r = requests.get( self.webservice_url + str(id), auth=self.auth, verify=self.ca_certificate_path )
+		if r.status_code == 200:
+			return (json.loads(r.text))['Value']
+		else:
+			return None
 
-    def GetPasswordByName(self,name):
-        return [ self.GetPassword(id) for id in self.FindIdByName(name) if self.GetPassword(id) ]
+	def GetPasswordByName(self,name):
+		return [ self.GetPassword(id) for id in self.FindIdByName(name) ]
+
+	def NewPassword(self,name,password,usage):
+		payload = {'Name': name, 'Value': password, 'Usage': usage}
+		r = requests.post( self.webservice_url, params=payload )
+		self.LoadPasswords()
+
+	def UpdatePassword(self,id,name,password,usage):
+		payload = {'Name': name, 'Value': password, 'Usage': usage}
+		r = requests.put( self.webservice_url + id, params=payload )
+		self.LoadPasswords()
