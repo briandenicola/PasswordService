617dedc7899577370db35b823eb304952e8aa0b9
Brian Denicola <bjd145@yahoo.com>
2015-09-15 23:08:22 -0500

    Fix tabing and spacing as well as more console.log
    Trying to track down occasional bug on HTTP Post when saving new
    Password

--- Source/PasswordService.Web.Ui/js/controllers.js
+++ Source/PasswordService.Web.Ui/js/controllers.js
@@ -1,115 +1,118 @@
 ﻿'use strict';
 
 (function (app) {
-    var passwordAppController = function( $scope ) {
-        $scope.searchTerm = '';
-	$scope.predicate = 'Name';
-	$scope.reverse = false;
-    }
+	var passwordAppController = function( $scope ) {
+		$scope.searchTerm = '';
+		$scope.predicate = 'Name';
+		$scope.reverse = false;
+	}
 
-    var passwordServiceController = function ($scope, passwordService) {
-	$scope.currentPage = 1;        
+	var passwordServiceController = function ($scope, passwordService) {
+		$scope.currentPage = 1;        
 
-        passwordService
-             .getAll()
-             .success(function (data) {
-                 $scope.passwords = data;
-             });
+		passwordService
+			 .getAll()
+			 .success(function (data) {
+				 $scope.passwords = data;
+			 });
 
-        $scope.showPassword = function (id) {
-            passwordService
-             .getById(id)
-             .success(function (data) {
-                  prompt("The password is...",data.Value.toString());
-             });  
-        };
-    }
+		$scope.showPassword = function (id) {
+			passwordService
+			 .getById(id)
+			 .success(function (data) {
+				  prompt("The password is...",data.Value.toString());
+			 });  
+		};
+	}
 
-    var passwordCreateModalController = function ($scope, $modal, $log) {
-      $scope.create = {
-        password: {
-            Name: "",
-            Value: "",
-            Usage: ""
-        }
-      };
+	var passwordCreateModalController = function ($scope, $modal, $log) {
+		$scope.create = {
+			password: {
+				Name: "",
+				Value: "",
+				Usage: ""
+			}
+		};
 
-      $scope.open = function (size) {
-		console.log("Scope - Open");
-        var modalInstance = $modal.open({
-          templateUrl: 'template/modal/create.html',
-          controller: 'passwordCreateServiceController',
-          size: size,
-          resolve: {
-            password: function () {
-              return $scope.create.password;
-            }
-          }
-        });
+		$scope.open = function (size) {
+			console.log("Scope - Open");
+			var modalInstance = $modal.open({
+				templateUrl: 'template/modal/create.html',
+				controller: 'passwordCreateServiceController',
+				size: size,
+				resolve: {
+					password: function () {
+						return $scope.create.password;
+					}
+				}
+			});
 
-        modalInstance.result.then(function (password) {
-			console.log("modalInstance.result.then");
-			console.log("password - " + password.Name);
-			console.log("password - " + password.Usage);
-			$scope.create.password = password;
-        });
-      }
-    }
+			modalInstance.result.then(function (password) {
+				console.log("Inside modalInstance.result.then with password Name - " + password.Name);
+				$scope.create.password = password;
+			});
+		}
+	}
 
-    var passwordCreateServiceController = function ($scope, $modalInstance, passwordService) {
+	var passwordCreateServiceController = function ($scope, $modalInstance, passwordService) {
 
-        $scope.cancel = function () {
-			console.log("Scope - cancel");
+		$scope.cancel = function () {
+			console.log("Scope - Cancel");
 			$modalInstance.dismiss('cancel');  
-        };
+		};
 
-        $scope.save = function () {
-			console.log("Scope - new/save " + $scope.create.password);
+		$scope.save = function () {
+			console.log("Scope - New/Save function with Password Name - " + $scope.create.password.Name);
 			createPassword();
 			$modalInstance.close($scope.create.password);
 			window.location.replace("");
-        }
+		}
 
-        var createPassword = function () {
-            passwordService.create($scope.create.password)
-                .success(function (password) {
-                    //$scope.create.password = null;
-                });
-        };
-    }
+		var createPassword = function () {
+			passwordService.create($scope.create.password)
+				.success(function () {
+					console.log("Success Call to HTTP POST - New/Save ");
+				})
+				.error(function () {
+					console.log("Erroring Calling to HTTP POST - New/Save ");
+				});
+		};
+	}
 
-    var passwordEditServiceController = function ($scope, $routeParams, passwordService) {
+	var passwordEditServiceController = function ($scope, $routeParams, passwordService) {
 
-        passwordService
-             .getById($routeParams.id)
-             .success(function (data) {
-                 $scope.password = data;
-             });
+		passwordService
+			 .getById($routeParams.id)
+			 .success(function (data) {
+	            $scope.password = data;
+			 });
 
-        $scope.cancel = function () {
+		$scope.cancel = function () {
 			window.location.replace(""); 
-        };
+		};
 
-        $scope.save = function () {
-			console.log("Scope - update/save Password " + $scope.password);
-            updatePassword();
-            window.location.replace("");
-        }
+		$scope.save = function () {
+		    console.log("Scope - Update/Save function with Password Name - " + $scope.create.password.Name);
+			updatePassword();
+			window.location.replace("");
+		}
 
-        var updatePassword = function () {
-			console.log("Scope - update Password " + $scope.password);
-            passwordService.update($scope.password)
-                .success(function () {
-                    //$scope.password = null;
-                    window.location.replace("");
-                });
-        };
-    }
+		var updatePassword = function () {
+			passwordService.update($scope.password)
+				.success(function () {
+				    console.log("Success Call to HTTP PUT - Update/Save ");
+					window.location.replace("");
+				})
+				.error(function () {
+				    console.log("Erroring Calling to HTTP PUTT - Update/Save ");
+				});
+		};
+	}
 
-    app.controller("passwordAppController", passwordAppController);
-    app.controller("passwordServiceController", passwordServiceController);
-    app.controller("passwordCreateModalController", passwordCreateModalController);
-    app.controller("passwordCreateServiceController", passwordCreateServiceController);    
-    app.controller("passwordEditServiceController", passwordEditServiceController);
+	app.controller("passwordAppController", passwordAppController);
+	app.controller("passwordServiceController", passwordServiceController);
+	app.controller("passwordCreateModalController", passwordCreateModalController);
+	app.controller("passwordCreateServiceController", passwordCreateServiceController);    
+	app.controller("passwordEditServiceController", passwordEditServiceController);
 
 }(angular.module("passwordServiceApp")));
